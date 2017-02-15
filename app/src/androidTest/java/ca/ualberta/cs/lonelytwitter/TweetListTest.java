a808c8c923eac7cd773b6b1f6881cbc24e61f189
Hanwen1 <hanwen1@ualberta.ca>
2017-02-14 19:25:44 -0700

    Add files via upload

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,56 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by hanwen1 on 2/14/17.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2{
+    public TweetListTest(){
+        super(LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
+
+    public void testGetTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("Another Test Tweet");
+
+        tweets.add(tweet);
+        Tweet retrunedTweet = tweets.getTweet(0);
+
+        assertEquals(retrunedTweet.getMessage(), tweet.getMessage());
+        assertEquals(retrunedTweet.getDate(), tweet.getDate());
+
+
+    }
+
+
+
+    public void testDeleteTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("Last tweet");
+
+        tweets.add(tweet);
+        tweets.delete(tweet);
+
+        assertFalse(tweets.hasTweet(tweet));
+
+
+    }
+
+    public void testStrings(){
+        assertEquals("'test' should be 'test'", "test", "test");
+        assertTrue("'test' should start with 't'", "test".startsWith("t"));
+    }
+
+
+
+}
