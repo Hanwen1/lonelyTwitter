51507826de4c2d78ff06bbee6d2ec72d8f00fbe3
Hanwen1 <hanwen1@ualberta.ca>
2017-03-03 15:40:02 -0700

    Update LonelyTwitterActivityTest.java

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -75,7 +75,25 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2<
         solo.goBack();
         solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
     }
+    public void testUI(){
+        LonelyTwitterActivity activity =  (LonelyTwitterActivity) solo.getCurrentActivity();
 
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+
+        solo.clickOnButton("Clear");
+        solo.enterText((EditText) solo.getView(R.id.body), "Test Tweet!");
+
+        solo.clickOnButton("Save");
+        solo.waitForText("Test Tweet!");
+
+        final ListView oldTweetsList = activity.getOldTweetsList();
+        Tweet tweet = (Tweet) oldTweetsList.getItemAtPosition(0);
+
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Wrong Activity", EditTweetActivity.class);
+
+        solo.waitForText("Test Tweet!");
+    }
 
     @Override
     public void tearDown() throws  Exception{
